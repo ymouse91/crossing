0f162bc3e8e20b7915a969b55a00fdff1889596d
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-11-20 12:12:41 +0200

    standalone ios

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'rc-v4';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './puzzles_7x5.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
